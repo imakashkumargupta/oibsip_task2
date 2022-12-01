55652f0f4fb30ae1ef7a81816eab5bc9517cb8eb
imakashkrgupta <imakashkrgupta@gmail.com>
2022-12-01 11:27:11 +0530

    oibsip_task2

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+function menu_show(){
+    document.getElementById("menu_on").style.display="none";
+    document.getElementById("menu_off").style.display="block";
+    document.getElementById("nav_links_id").style.display="block";
+}
+function menu_hide(){
+    document.getElementById("menu_on").style.display="block";
+    document.getElementById("menu_off").style.display="none";
+    document.getElementById("nav_links_id").style.display="none";
+}
